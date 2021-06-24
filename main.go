57d9d758c29c8e86a3a46840106d38408cdaa6d3
Steve Yackey <syackey@patriotsoftware.com>
2021-06-24 10:43:31 -0400

    Testing using GHA.Fatalf

--- main.go
+++ main.go
@@ -18,29 +18,26 @@ func main() {
 
 	client, err := NewClient()
 	if err != nil {
-		panic("Could not create client")
+		githubactions.Fatalf("Could not create client")
 	}
 
 	bot := &Bot{client}
 	_ = bot
 	if err != nil {
-		fmt.Printf("Error %+v: ", err)
-		os.Exit(1)
+		githubactions.Fatalf("Error %+v: \n", err)
 	}
 
 	_, err = bot.TestAuth()
 	if err != nil {
-		fmt.Printf("Unable to authenticate. Check your .slack_token file. Error: %+v\n", err)
-		os.Exit(1)
+		githubactions.Fatalf("Unable to authenticate. Check your .slack_token file. Error: %+v\n", err)
 	}
 
 	_ = bot.PostMessage(destination, message)
 
 	if err != nil {
-		fmt.Printf("Oh no! We can't post a message! %+v", err)
+		githubactions.Fatalf("Oh no! We can't post a message! %+v", err)
 	}
 
 	message = fmt.Sprintf("Message sent to %s.\n", destination)
-	fmt.Printf("::set-output name=validate-output::%s \n", message)
 	githubactions.SetOutput("validate-output", message)
 }
