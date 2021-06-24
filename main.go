9a16f1fa8fb4c125a5ff6f3967d1184bf5432275
Steve Yackey <syackey@patriotsoftware.com>
2021-06-24 13:40:42 -0400

    Updated with global export

--- main.go
+++ main.go
@@ -19,28 +19,28 @@ func main() {
 
 	client, err := NewClient()
 	if err != nil {
-		//githubactions.Fatalf("Could not create client")
-		gjs.Global.Call("process.exit", 1)
+		fmt.Fprintf(os.Stderr, "Could not create client")
+		gjs.Module.Call("process.exit", 1)
 	}
 
 	bot := &Bot{client}
 	_ = bot
 	if err != nil {
-		//githubactions.Fatalf("Error %+v: \n", err)
-		gjs.Global.Call("process.exit", 1)
+		fmt.Fprintf(os.Stderr, "Error %+v: \n", err)
+		gjs.Module.Call("process.exit", 2)
 	}
 
 	_, err = bot.TestAuth()
 	if err != nil {
-		//githubactions.Fatalf("Unable to authenticate. Check your .slack_token file. Error: %+v\n", err)
-		gjs.Global.Call("process.exit", 1)
+		fmt.Fprintf(os.Stderr, "Unable to authenticate. Check your .slack_token file. Error: %+v\n", err)
+		gjs.Global.Call("ExitAndFail", 3)
 	}
 
 	_ = bot.PostMessage(destination, message)
 
 	if err != nil {
-		//githubactions.Fatalf("Oh no! We can't post a message! %+v", err)
-		gjs.Global.Call("process.exit", 1)
+		fmt.Fprintf(os.Stderr, "Oh no! We can't post a message! %+v", err)
+		gjs.Global.Call("ExitAndFail", 4)
 	}
 
 	message = fmt.Sprintf("Message sent to %s.\n", destination)
