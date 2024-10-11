22338d1a17c2200e06bf6bb509f0773a1766237e
Janelle Archer <jarcher@patriotsoftware.com>
2024-10-11 16:09:18 -0400

    test

--- parser.py
+++ parser.py
@@ -1,19 +1,19 @@
 import sys
 
-def newResultLine(jobresult):
+# def newResultLine(jobresult):
     
-    match(jobresult.split(':')[1]):
-        case "success":
-            return f"✅ {jobresult.split(':')[0]} Succeeded.\n"
-        case "failure":
-            return f"❌ {jobresult.split(':')[0]} Failed. \n"
-        case _:
-            return f"❕ {jobresult.split(':')[0]} Didn't Run. \n"
+#     match(jobresult.split(':')[1]):
+#         case "success":
+#             return f"✅ {jobresult.split(':')[0]} Succeeded.\n"
+#         case "failure":
+#             return f"❌ {jobresult.split(':')[0]} Failed. \n"
+#         case _:
+#             return f"❕ {jobresult.split(':')[0]} Didn't Run. \n"
            
-def format_results(inputString):
-    jobresult_list = inputString.splitlines()
-    formatted_list = [newResultLine(result) for result in inputString.splitlines()]
-    print (''.join(formatted_list))
+# def format_results(inputString):
+#     jobresult_list = inputString.splitlines()
+#     formatted_list = [newResultLine(result) for result in inputString.splitlines()]
+#     print (''.join(formatted_list))
 
 # format_results(sys.argv[1])
 print(sys.argv[0])
