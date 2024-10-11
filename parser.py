188b7582388e1143461c7cf01bcb91395069bb5f
Janelle Archer <jarcher@patriotsoftware.com>
2024-10-11 10:38:42 -0400

    test

--- parser.py
+++ parser.py
@@ -1,20 +1,20 @@
 import sys
 
-# def newResultLine(jobresult):
+def newResultLine(jobresult):
     
-#     match(jobresult.split(':')[1]):
-#         case "success":
-#             return f"✅ {jobresult.split(':')[0]} Succeeded.\n"
-#         case "failure":
-#             return f"❌ {jobresult.split(':')[0]} Failed. \n"
-#         case _:
-#             return f"❕ {jobresult.split(':')[0]} Didn't Run. \n"
+    match(jobresult.split(':')[1]):
+        case "success":
+            return f"✅ {jobresult.split(':')[0]} Succeeded.\n"
+        case "failure":
+            return f"❌ {jobresult.split(':')[0]} Failed. \n"
+        case _:
+            return f"❕ {jobresult.split(':')[0]} Didn't Run. \n"
            
 def format_results(inputString):
-#   jobresult_list = inputString.splitlines()
-#   formatted_list = [newResultLine(result) for result in inputString.splitlines()]
-#   return ''.join(formatted_list)
-  
-    print(inputString)
+  jobresult_list = inputString.splitlines()
+  formatted_list = [newResultLine(result) for result in inputString.splitlines()]
+  return ''.join(formatted_list)
+
+
 format_results(sys.argv[1])
 
