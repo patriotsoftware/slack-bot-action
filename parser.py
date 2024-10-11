9306ee06ec7c8dbda65911b8ceace7e30514828d
Janelle Archer <jarcher@patriotsoftware.com>
2024-10-11 09:41:46 -0400

    more test

--- parser.py
+++ parser.py
@@ -1,14 +1,14 @@
 import sys
 
-def newResultLine(jobresult):
+# def newResultLine(jobresult):
     
-    match(jobresult.split(':')[1]):
-        case "success":
-            return f"✅ {jobresult.split(':')[0]} Succeeded.\n"
-        case "failure":
-            return f"❌ {jobresult.split(':')[0]} Failed. \n"
-        case _:
-            return f"❕ {jobresult.split(':')[0]} Didn't Run. \n"
+#     match(jobresult.split(':')[1]):
+#         case "success":
+#             return f"✅ {jobresult.split(':')[0]} Succeeded.\n"
+#         case "failure":
+#             return f"❌ {jobresult.split(':')[0]} Failed. \n"
+#         case _:
+#             return f"❕ {jobresult.split(':')[0]} Didn't Run. \n"
            
 def format_results(inputString):
 #   jobresult_list = inputString.splitlines()
