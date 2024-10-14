b90cfe468dfb12155fb8ea22646b0f2a7e185bdc
Janelle Archer <jarcher@patriotsoftware.com>
2024-10-14 10:35:31 -0400

    test

--- test.sh
+++ test.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+          #!/bin/bash
+          INPUT_DESTINATION="committer"
+          EMAIL="jarcher@patriotsoftware.com"
 
-output=$(python parser.py)
-echo $output
+          if [[ "$INPUT_DESTINATION" == "#"* ]]; then
+            echo "1"
+         
+          elif [[ "$INPUT_DESTINATION" == "committer" && "$EMAIL" = *'@patriotsoftware.com' ]]; then
+            echo "2"
+            
+          else 
+            echo "3"
+           
+          fi
