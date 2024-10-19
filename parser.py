1d31e83763680e2167cdaa9bb926d12e7252a747
Janelle <jarcher@patriotsoftware.com>
2024-10-18 21:19:53 -0400

    Use python and bash to send info to official action (#29)
    Remove golang and move to python/bash composite action.

---
+++ parser.py
@@ -0,0 +1,21 @@
+import sys
+
+def newResultLine(jobresult):   
+    match(jobresult.split(':')[1]):
+        case "success":
+            return f"✅ {jobresult.split(':')[0]} Succeeded.\n"
+        case "failure":
+            return f"❌ {jobresult.split(':')[0]} Failed. \n"
+        case _:
+            return f"❕ {jobresult.split(':')[0]} Didn't Run. \n"
+           
+def format_results(inputString):
+    if not inputString:
+       sys.exit()
+    jobresult_list = inputString.splitlines()
+    formatted_list = [newResultLine(result) for result in inputString.splitlines()]
+    print (''.join(formatted_list))
+
+format_results(sys.argv[1])
+
+
